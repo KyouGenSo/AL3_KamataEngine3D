73d469fe1d9081ee575e2f5d4d444857b7861150
KyouGenSo <k023g0083@g.neec.ac.jp>
2024-05-15 15:39:26 +0900

    03_07_ex2完成

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -2,7 +2,7 @@
 
 Enemy::Enemy() {}
 
-Enemy::~Enemy() {}
+Enemy::~Enemy() { delete enemyPhase_; }
 
 void Enemy::Initialize(Model* model, uint32_t textureHandle) {
 
@@ -17,45 +17,72 @@ void Enemy::Initialize(Model* model, uint32_t textureHandle) {
 	worldTransform_.translation_ = position_;
 
 	velocity_ = {0.0f, 0.0f, -0.5f};
+
+	enemyPhase_ = new EnemyPhaseAproach();
 }
 
 void Enemy::Update() {
 
-	//switch (phase_) {
-	//case Phase::Aproach:
+	// switch (phase_) {
+	// case Phase::Aproach:
 	//	Aproach();
 	//	break;
-	//case Phase::Leave:
+	// case Phase::Leave:
 	//	Leave();
 	//	break;
-	//}
+	// }
 
-	(this->*pPhaseFunctionTable_[static_cast<size_t>(phase_)])();
+	//(this->*pPhaseFunctionTable_[static_cast<size_t>(phase_)])();
+
+	enemyPhase_->Update(this);
 
 	worldTransform_.UpdateMatrix();
 }
 
 void Enemy::Draw(const ViewProjection& viewProjection) { model_->Draw(worldTransform_, viewProjection, textureHandle_); }
 
-void Enemy::Aproach() {
-	velocity_ = {0.0f, 0.0f, -0.5f};
-	worldTransform_.translation_ += velocity_;
+// void Enemy::Aproach() {
+//	velocity_ = {0.0f, 0.0f, -0.5f};
+//	worldTransform_.translation_ += velocity_;
+//
+//	if (worldTransform_.translation_.z <= 0.0f) {
+//		phase_ = Phase::Leave;
+//	}
+// }
+//
+// void Enemy::Leave() {
+//	velocity_ = {0.5f, 0.5f, 0.0f};
+//	worldTransform_.translation_ += velocity_;
+//
+//	if (worldTransform_.translation_.x >= 10.0f) {
+//		phase_ = Phase::Aproach;
+//	}
+// }
 
-	if (worldTransform_.translation_.z <= 0.0f) {
-		phase_ = Phase::Leave;
+//void (Enemy::*const Enemy::pPhaseFunctionTable_[])() = {
+//    &Enemy::Aproach, // 要素番号0
+//    &Enemy::Leave,   // 要素番号1
+//};
+
+void Enemy::ChangePhase(BaseEnemyPhase* phase) {
+	delete enemyPhase_;
+	enemyPhase_ = phase;
+}
+
+void EnemyPhaseAproach::Update(Enemy* enemy) {
+	Vector3 velocity_ = {0.0f, 0.0f, -0.5f};
+	enemy->SetTranlation(velocity_);
+
+	if (enemy->GetTranslationZ() <= 0.0f) {
+		enemy->ChangePhase(new EnemyPhaseLeave());
 	}
 }
 
-void Enemy::Leave() {
-	velocity_ = {0.5f, 0.5f, 0.0f};
-	worldTransform_.translation_ += velocity_;
+void EnemyPhaseLeave::Update(Enemy* enemy) {
+	Vector3 velocity_ = {0.5f, 0.5f, 0.0f};
+	enemy->SetTranlation(velocity_);
 
-	if (worldTransform_.translation_.x >= 10.0f) {
-		phase_ = Phase::Aproach;
+	if (enemy->GetTranslationX() >= 10.0f) {
+		enemy->ChangePhase(new EnemyPhaseAproach());
 	}
 }
-
-void(Enemy::* const Enemy::pPhaseFunctionTable_[])() = {
-	&Enemy::Aproach, // 要素番号0
-	&Enemy::Leave,	// 要素番号1
-};
--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -1,9 +1,31 @@
 #pragma once
+#include "Input.h"
 #include "Matrix4x4Function.h"
 #include "Model.h"
 #include "WorldTransform.h"
 #include <algorithm>
-#include "Input.h"
+
+class Enemy;
+
+class BaseEnemyPhase {
+
+public:
+	virtual ~BaseEnemyPhase() = default;
+	virtual void Update(Enemy* enemy) = 0;
+
+};
+
+class EnemyPhaseAproach : public BaseEnemyPhase {
+public:
+	void Update(Enemy* enemy) override;
+
+};
+
+class EnemyPhaseLeave : public BaseEnemyPhase {
+public:
+	void Update(Enemy* enemy) override;
+
+};
 
 class Enemy {
 
@@ -12,16 +34,28 @@ public:
 	~Enemy();
 
 	void Initialize(Model* model, uint32_t textureHandle);
+
 	void Update();
+
+	void ChangePhase(BaseEnemyPhase* phase);
+
 	void Draw(const ViewProjection& viewProjection);
 
 	void Aproach();
 	void Leave();
 
-private:
+	//------------------------------------------------------------------------------------------------
+	void SetTranlation(const Vector3& velocity) { worldTransform_.translation_ += velocity; }
 
+	float GetTranslationX() const { return worldTransform_.translation_.x; }
+	float GetTranslationY() const { return worldTransform_.translation_.y; }
+	float GetTranslationZ() const { return worldTransform_.translation_.z; }
+
+private:
 	// 関数ポインター配列
-	static void(Enemy::* const pPhaseFunctionTable_[])();
+	static void (Enemy::*const pPhaseFunctionTable_[])();
+
+	BaseEnemyPhase* enemyPhase_;
 
 	WorldTransform worldTransform_;
 	Model* model_ = nullptr;
