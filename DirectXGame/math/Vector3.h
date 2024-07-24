110b19ba579bc496cacc29681e44b0cd100871fe
KyouGenSo <k023g0083@g.neec.ac.jp>
2024-07-24 15:21:05 +0900

    a

--- DirectXGame/math/Vector3.h
+++ DirectXGame/math/Vector3.h
@@ -7,4 +7,13 @@ struct Vector3 final {
 	float x;
 	float y;
 	float z;
+
+	Vector3 operator+=(const Vector3& v) {
+		Vector3 result;
+		x += v.x;
+		y += v.y;
+		z += v.z;
+
+		return result;
+	}
 };
